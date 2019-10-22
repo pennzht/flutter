49eb83f5c520c2dc996734195f0776928e83c7f6
Tianguang Zhang <penn.zht@gmail.com>
2019-10-22 11:44:36 -0700

    Further playing around.

--- examples/flutter_gallery/lib/demo/shrine/scrim.dart
+++ examples/flutter_gallery/lib/demo/shrine/scrim.dart
@@ -12,27 +12,29 @@ class Scrim extends StatefulWidget {
 }
 
 class _ScrimState extends State<Scrim> with TickerProviderStateMixin {
-  AnimationController _controller;
 
   @override
   void initState() {
     super.initState();
-    _controller = AnimationController(
-      duration: const Duration(milliseconds: 500),
-      vsync: this,
-    );
   }
 
   @override
   void dispose() {
-    _controller.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
+    print('Scrim.widget.controller = ${widget.controller}');
     final Size deviceSize = MediaQuery.of(context).size;
-    print(_controller.value);
-    return Container(width: deviceSize.width, height: deviceSize.height * _controller.value, color: Colors.blue);
+    print('Scrim.widget.controller.value = ${widget.controller.value}');
+    return AnimatedBuilder(
+      animation: widget.controller,
+      builder: (BuildContext context, Widget child) {
+        return Container(width: deviceSize.width, height: deviceSize.height * widget.controller.value, color: Colors.blue);
+      }
+    );
+
+      //Container(width: deviceSize.width, height: deviceSize.height * _controller.value, color: Colors.blue);
   }
 }
