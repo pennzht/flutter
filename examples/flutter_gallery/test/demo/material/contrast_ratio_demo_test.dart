cb77411ca908762cd40c9cb3810411f4a49e2e8d
Tianguang Zhang <penn.zht@gmail.com>
2020-01-14 16:46:47 -0500

    Add test.

--- examples/flutter_gallery/test/demo/material/contrast_ratio_demo_test.dart
+++ examples/flutter_gallery/test/demo/material/contrast_ratio_demo_test.dart
@@ -2,13 +2,30 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:flutter/material.dart';
 import 'package:flutter_gallery/demo/material/contrast_ratio_demo.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  testWidgets('Contrast ratio is correct.', (WidgetTester tester) async {
-    await tester.pumpWidget(ContrastRatioDemo());
+  testWidgets('Unmerged good demo.', (WidgetTester tester) async {
+    await tester.pumpWidget(ContrastRatioDemoUnmergedGood());
+
+    expect(tester, meetsGuideline(textContrastGuideline));
+  });
+
+  testWidgets('Unmerged bad demo.', (WidgetTester tester) async {
+    await tester.pumpWidget(ContrastRatioDemoUnmergedBad());
+
+    expect(tester, meetsGuideline(textContrastGuideline));
+  });
+
+  testWidgets('Merged good demo.', (WidgetTester tester) async {
+    await tester.pumpWidget(ContrastRatioDemoMergedGood());
+
+    expect(tester, meetsGuideline(textContrastGuideline));
+  });
+
+  testWidgets('Merged bad demo.', (WidgetTester tester) async {
+    await tester.pumpWidget(ContrastRatioDemoMergedBad());
 
     expect(tester, meetsGuideline(textContrastGuideline));
   });
