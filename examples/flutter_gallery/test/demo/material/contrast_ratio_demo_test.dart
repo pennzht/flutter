657d2aae2eeea76845b34fe77dad4853f650c5e7
Tianguang Zhang <penn.zht@gmail.com>
2020-01-14 16:29:24 -0500

    Add test.

---
+++ examples/flutter_gallery/test/demo/material/contrast_ratio_demo_test.dart
@@ -0,0 +1,15 @@
+// Copyright 2014 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+import 'package:flutter_gallery/demo/material/contrast_ratio_demo.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('Drawer header does not scroll', (WidgetTester tester) async {
+    await tester.pumpWidget(ContrastRatioDemo());
+
+    expect(tester, meetsGuideline(textContrastGuideline));
+  });
+}
